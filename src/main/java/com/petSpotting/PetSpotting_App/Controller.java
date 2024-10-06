dba6ca162e137aeee048dcc49b436574c555042c
Hortensjaa <lilka305@gmail.com>
2024-10-06 13:52:14 +0200

    photo -> google driver backend

---
+++ src/main/java/com/petSpotting/PetSpotting_App/Controller.java
@@ -0,0 +1,30 @@
+package com.petSpotting.PetSpotting_App;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private DriverService service;
+
+    @PostMapping("/uploadToGoogleDrive")
+    public Object handleFileUpload(@RequestParam("image") MultipartFile file) throws IOException, GeneralSecurityException {
+        if (file.isEmpty()) {
+            return "FIle is empty";
+        }
+        File tempFile = File.createTempFile("temp", null);
+        file.transferTo(tempFile);
+        Res res = service.uploadImageToDrive(tempFile);
+        System.out.println(res);
+        return res;
+    }
+}
